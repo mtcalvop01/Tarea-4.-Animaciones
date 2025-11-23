1135a00ae7111cce7b9be8ab3641c1bd84064aa5
mtcalvop01 <mtcalvop01@iesalbarregas.es>
2025-11-24 00:25:09 +0100

    🔄️Update: Actualización del para cambiar el nombre de una clase y añadir una imagen, en el style.css he modificado algunos estilos de algunas clases y en el script.js he añadido las animaciones que faltaban

--- script.js
+++ script.js
@@ -1,17 +1,46 @@
 gsap.registerPlugin(ScrollTrigger);
 
-gsap.fromTo("#hero", {
-opacity:1
-},{
+gsap.fromTo("#hero", 
+  { opacity: 1 }, 
+  { 
+    opacity: 0,
     scrollTrigger: {
-              trigger: "header",
-              start: "50% 50%",
-              end: "bottom 20%",
-              scrub: 1,
-              markers: false
-    },
-        opacity: 0
-    });
+      trigger: "header",
+      start: "50% 50%",
+      end: "bottom 20%",
+      scrub: 1,
+      markers: false
+    }
+  }
+);
+
+gsap.from("#hero h1", { 
+  y: -50, 
+  opacity: 0, 
+  duration: 1 
+});
+
+gsap.from("#hero p", { 
+  y: -30, 
+  opacity: 0, 
+  duration: 1, 
+  delay: 0.5 
+});
+
+
+gsap.to("#titulo", {
+  y: -50,
+  opacity: 1,
+  ease: "none",
+  scrollTrigger: {
+    trigger: "#titulo",
+    start: "top 80%", 
+    end: "top 50%",
+    scrub: 1,
+    markers: false
+  }
+});
+
 
 gsap.to("#letras", {
    y: -10,            
@@ -23,10 +52,10 @@ gsap.to("#letras", {
 gsap.from(".tarjeta", {
   scrollTrigger: {
     trigger: "#titulo",
-    start: "70% 30%",   // cuando el hero está visible
+    start: "70% 30%",  
     end: "bottom 0%",
     scrub: 1,
-    markers: true
+    markers: false
   },
   x: -300,   // empieza fuera de la pantalla a la izquierda
   opacity: 0,
@@ -35,6 +64,47 @@ gsap.from(".tarjeta", {
 });
 
 
+gsap.from("#servicios-destacados", {
+  scrollTrigger: {
+    trigger: ".tarjeta",
+    start: "bottom -10%",
+    end: "bottom 10%",
+    scrub: 1,
+    markers: false
+  },
+  rotation: 360,        
+  ease: "none"
+})
+
+
+gsap.from(".servicio", {
+  scrollTrigger: {
+    trigger: "#caracteristicas",
+    start: "90% 30%",   
+    end: "bottom 0%",
+    scrub: 1,
+    markers: false
+  },
+  x: -300,   
+  opacity: 0,
+  duration: 4,
+  stagger: 0.8   // cada tarjeta aparece con un pequeño retraso
+});
+
+gsap.to(".comentario", {
+  scale: 1.05,        
+  duration: 0.9,       
+  yoyo: true,          
+  ease: "power1.inOut",
+  repeat: 1,
+  scrollTrigger: {
+    trigger: ".servicio", 
+    start: "bottom -20%",          
+    end: "bottom 0%",
+    toggleActions: "play none none reset", // cada vez que entras, vuelve a jugar                 
+    markers: false
+  }
+});
 
 gsap.fromTo("#footer", {
     opacity:0
@@ -48,4 +118,5 @@ gsap.fromTo("#footer", {
     },
     opacity: 1
 });
+
 
